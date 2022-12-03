ee236ead51b0c5b655cee5d4ea081e8bcd4261b3
fsossai <federico.sossai@gmail.com>
2022-12-03 04:52:50 -0600

    Added `vscode.sh`

---
+++ vscode.sh
@@ -0,0 +1,24 @@
+# obtained with:
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension gurumukhi.selected-lines-count
+code --install-extension kaiwood.center-editor-window
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension ms-vscode.makefile-tools
+code --install-extension ms-vscode.remote-explorer
+code --install-extension RReverser.llvm
+code --install-extension twxs.cmake
+
