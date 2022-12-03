d39f59c4703abde3ca0724b2d21415ca4a83151d
fsossai <federico.sossai@gmail.com>
2022-12-03 04:57:58 -0600

    Added `git.sh`

---
+++ git.sh
@@ -0,0 +1,6 @@
+git config core.fileMode false
+git config --global user.name 'fsossai'
+git config --global user.email 'federico.sossai@gmail.com'
+git config --global alias.gl 'log --oneline --graph'
+git config --global alias.gla 'log --oneline --graph --all'
+
