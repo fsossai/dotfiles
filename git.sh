d70394d51892874c3d87a4c3bbcf89a208e1c5d5
fsossai <federico.sossai@gmail.com>
2022-12-07 17:41:41 -0600

    Compact path in `PS1`

--- git.sh
+++ git.sh
@@ -3,4 +3,5 @@ git config --global user.name 'fsossai'
 git config --global user.email 'federico.sossai@gmail.com'
 git config --global alias.gl 'log --oneline --graph'
 git config --global alias.gla 'log --oneline --graph --all'
+git config --global alias.diffn 'diff --name-only'
 
