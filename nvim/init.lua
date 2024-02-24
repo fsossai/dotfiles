ada50acfcc51dc00da69d1898aefb4a61ea4658b
fsossai <federico.sossai@gmail.com>
2024-02-23 21:06:52 -0600

    Removed lua experimental loader

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,11 @@
-vim.loader.enable()
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
-    "--filter = blob:none",
+    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch = stable", -- latest stable release
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
